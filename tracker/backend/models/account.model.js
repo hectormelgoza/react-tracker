61d105bb2b1bef8c7a0923976db86d39b6b98173
Hector Melgoza <h.melgoza.4@gmail.com>
2020-06-02 18:23:31 -0700

    initial backend

---
+++ tracker/backend/models/account.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema({
+  name: { type: String, required: true },
+  user: { type: String, required: true },
+  password: { type: String, required: true },
+  date: { type: Date, required: true },
+}, {
+  timestamps: true,
+});
+
+const Account = mongoose.model('account', accountSchema);
+
+module.exports = Account;
