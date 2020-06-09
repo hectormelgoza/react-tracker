20fba9f88189c3dc749b6dd50fbce18514c00352
Hector Melgoza <h.melgoza.4@gmail.com>
2020-06-08 19:24:05 -0700

    main app with Router

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import {Route, Link, BrowserRouter as Router} from 'react-router-dom'
-import Welcome  from './components/Welcome'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+
 import Add from './components/Add'
+import Navbar from "./components/Navbar"
 import ShowAccounts from './components/ShowAccounts'
 import './App.css';
 
 function App() {
   return (
-      <div className="main-container">
-        <Welcome />
-        <Router>
-        <Link className="add-btn" to="/account">Add Account</Link>
-        <Route path="/" component={ShowAccounts} />
-        <Route path="/accounts" component={Add} />
-        </Router>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <Route path="/" exact component={ShowAccounts} />
+      <Route path="/new" component={Add} />
       </div>
+    </Router>
   );
 }
 
