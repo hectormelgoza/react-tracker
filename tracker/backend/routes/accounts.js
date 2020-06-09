362373e87f86a9ef8eedb2678e1541381d85a768
Hector Melgoza <h.melgoza.4@gmail.com>
2020-06-08 19:21:57 -0700

    fixed backend

--- tracker/backend/routes/accounts.js
+++ tracker/backend/routes/accounts.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 
 const Account = require('../models/account.model');
-
 router.route('/').get((req, res) => {
   Account.find()
-    .then(accounts => {
-      return res.json(accounts);
-    })
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then(hatt => res.json(hatt))
+    .catch(err => res.status(400).json('Error: Did not find anything!' + err));
 });
 
 router.route('/add').post((req, res) => {
@@ -16,12 +13,12 @@ router.route('/add').post((req, res) => {
   const password = req.body.password;
   const date = Date.parse(req.body.date);
 
+
   const newAccount = new Account({
     name,
     user,
     password,
     date
-    
   });
 
   newAccount.save()
@@ -35,11 +32,11 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 }); */
 
-router.route('/:id').delete((req, res) => {
+/* router.route('/:id').delete((req, res) => {
   Account.findByIdAndDelete(req.params.id)
     .then(() => res.json('Account deleted!!'))
     .catch(err => res.status(400).json('Error: ' + err));
-});
+}); */
 
 /* router.route('/update/:id').post((req, res) => {
   Exercise.findById(req.params.id)
--- tracker/backend/server.js
+++ tracker/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 require('dotenv').config();
 
@@ -8,19 +9,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(morgan('tiny'));
 app.use(express.json());
 
 const uri = "mongodb+srv://hectormelgoza:thetrailblaze@cluster0-h4g1e.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("MongoDB CONNECTED");
 })
 
-const accounts = require('./routes/accounts.js');
+const routes = require('./routes/accounts.js');
 
-app.use('/accounts', accounts);
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
