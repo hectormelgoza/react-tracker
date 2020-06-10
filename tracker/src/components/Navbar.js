fed6124ad3127624d859a445dca82d9b020ad4c7
Hector Melgoza <h.melgoza.4@gmail.com>
2020-06-09 22:48:23 -0700

    initial navbar

---
+++ tracker/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Password Manager</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Accounts</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/new" className="nav-link">Add</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
