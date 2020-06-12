0018d6dee07f021c9340eff7cfe919b9df978c92
Hector Melgoza <h.melgoza.4@gmail.com>
2020-06-11 23:02:44 -0700

    now renders accounts using map

--- tracker/src/components/ShowAccounts.js
+++ tracker/src/components/ShowAccounts.js
@@ -2,70 +2,67 @@ import React, { Component } from 'react'
 /* import { Link } from 'react-router-dom' */
 import axios from 'axios'
 
-const Acc = props => (
+/* const Acc = props => (
     <tr>
-      <td>{props.account.name}</td>
-      <td>{props.account.user}</td>
-      <td>{props.account.password}</td>
-      <td>{props.account.date.substring(0,10)}</td>
+      <td>{props.accounts.name}</td>
+      <td>{props.accounts.user}</td>
+      <td>{props.accounts.password}</td>
+      <td>{props.accounts.date.substring(0,10)}</td>
       <td>
-        {/* <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a> */}
-      </td>
-    </tr>
-  )
-  
-  export default class ShowAccount extends Component {
+        { *//* <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a> */
+   
+       
+  export default class ShowAccounts extends Component {
+    
     constructor(props) {
       super(props);
-  
-      this.deleteAccount = this.deleteAccount.bind(this);
-  
+      /* this.deleteAccount = this.deleteAccount.bind(this); */
       this.state = {accounts: []};
     }
-  
+    
     componentDidMount() {
-      axios.get('http://localhost:5000/accounts/')
-        .then(response => {
-          this.setState({ accounts: response.data })
+      axios.get('http://localhost:4000/api')
+        .then(res => {
+        this.setState({accounts: res.data})
         })
         .catch((error) => {
           console.log(error);
         })
     }
   
-    deleteAccount(id) {
+   /*  deleteAccount(id) {
       axios.delete('http://localhost:5000/accounts/'+id)
         .then(response => { console.log(response.data)});
   
       this.setState({
         accounts: this.state.accounts.filter(el => el._id !== id)
       })
-    }
+    } */
   
-    accountList() {
-      return this.state.accounts.map(currentaccount => {
-        return <Acc exercise={currentaccount} deleteAccount={this.deleteAccount} key={currentaccount._id}/>;
-      })
+  /*   accountList(){
+      const account = this.state.accounts
+     
+      
+      console.log(account)
+    }; */
+    
+    showSensitive(){
+      console.log("you flipped")
     }
-  
+
     render() {
+      
+      console.log(this.state.accounts)
+      /* onst accountCards = {this.state.accounts}; */
       return (
         <div>
-          <h3>Logged Exercises</h3>
-          <table className="table">
-            <thead className="thead-light">
-              <tr>
-                <th>Username</th>
-                <th>Description</th>
-                <th>Duration</th>
-                <th>Date</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              { this.accountList() }
-            </tbody>
-          </table>
+          <h3>Accounts</h3>
+          {
+            this.state.accounts.map((item) => {
+            return <div className="account-item" key={item._id}><h4>{item.name}</h4> <br></br> 
+                  <button className="btn-view" onClick={this.showSensitive} >view</button></div>
+            })
+          }
         </div>
       )
     }
