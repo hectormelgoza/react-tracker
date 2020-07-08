51c4258fe5e24736354144eff839fde18ebdcce6
Hector Melgoza <h.melgoza.4@gmail.com>
2020-07-08 13:28:25 -0700

    first working version of deleting account from UI

--- tracker/src/components/ShowAccounts.js
+++ tracker/src/components/ShowAccounts.js
@@ -3,41 +3,42 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Item from './Item'
        
-  export default class ShowAccounts extends Component {
-    
-    constructor(props) {
-      super(props);
-      this.state = {accounts: []};
-      this.deleteAccount = this.deleteAccount.bind(this);
-    }
-    
-    componentDidMount() {
-      axios.get('http://localhost:4000/api')
-        .then(res => {
-        this.setState({accounts: res.data})
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-    }
+export default class ShowAccounts extends Component {
   
-    deleteAccount(id) {
-      axios.delete('http://localhost:4000/api/'+id)
-        .then(() => this.setState({
-          accounts: [this.state.accounts.filter(el => el !== id)]
-        }) );
-    }
-
-    render() {
-      console.log(this.state.accounts)
-      return (
-        <>
-          <h3>Accounts</h3>
-          <div>{
-          this.state.accounts.map(item => {
-          return <Item key={item._id} acc={item} delete={this.deleteAccount} /> })
-          }</div>
-        </>
-      )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {account: []};
+    this.deleteAccount = this.deleteAccount.bind(this);
   }
+  
+  componentDidMount() {
+    axios.get('http://localhost:4000/api')
+      .then(res => {
+      this.setState({account: res.data})
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteAccount(id) {
+    axios.delete('http://localhost:4000/api/'+id)
+    .then(() => console.log('account deleted from database'))
+    axios.get('http://localhost:4000/api')
+      .then(res => {
+      this.setState({account: res.data})
+      })
+  }
+
+  render() {
+    console.log(this.state.account)
+    return (
+      <div>
+        {
+        this.state.account.map(item => (
+          <Item key={item._id} acc={item} delete={() => this.deleteAccount(item._id)} /> ))
+        }
+      </div>
+    )
+  }
+}
