1c1740c477be09e05334c3f62438cbc3f9e5adf0
Hector Melgoza <h.melgoza.4@gmail.com>
2020-07-08 13:26:50 -0700

    changed onClick to run delete prop

--- tracker/src/components/Item.js
+++ tracker/src/components/Item.js
@@ -1,9 +1,6 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 
 export default class Item extends Component {
-    handleDelete(id){
-        this.props.delete(id);
-    }
 
 render() {
     return (
@@ -11,7 +8,7 @@ render() {
             <h3>{this.props.acc.name}</h3>
             <h5>User: {this.props.acc.user}</h5>
             <h5>Password: {this.props.acc.password}</h5>
-            <button className="btn-delete" onClick={() => {this.handleDelete(this.props.acc._id)}}>Delete</button>
+            <button className="btn-delete" onClick={() => {this.props.delete(this.props.acc._id)}}>Delete</button>
         </div>
     )
 }
