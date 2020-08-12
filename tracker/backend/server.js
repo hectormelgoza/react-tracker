fca4bf145fd50e63cfd7cce7222ae29d76c26044
Hector Melgoza <h.melgoza.4@gmail.com>
2020-08-12 16:36:31 -0700

    server updates

--- tracker/backend/server.js
+++ tracker/backend/server.js
@@ -2,18 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const passport = require("passport");
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 
-const uri = "mongodb+srv://hectormelgoza:thetrailblaze@cluster0-h4g1e.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -21,11 +29,11 @@ connection.once('open', () => {
 })
 
 const routes = require('./routes/accounts.js');
-const login = require('./routes/user.js');
+const users = require('./routes/user.js');
 
 
 app.use('/api', routes);
-app.use('/login', login);
+app.use('/api/users', users);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
