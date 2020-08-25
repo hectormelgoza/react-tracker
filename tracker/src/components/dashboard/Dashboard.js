bc9b96b96486ad551c8ae906921919906bf5de0f
Hector Melgoza <h.melgoza.4@gmail.com>
2020-08-24 21:11:42 -0700

    dashboard page

--- tracker/src/components/dashboard/Dashboard.js
+++ tracker/src/components/dashboard/Dashboard.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Add from '../Add'
+
+import { Link } from "react-router-dom";
 
 class Dashboard extends Component {
   onLogoutClick = e => {
@@ -13,28 +16,22 @@ class Dashboard extends Component {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div  className="dash-container">
         <div className="row">
-          <div className="landing-copy col s12 center-align">
+          <div className="">
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
             </h4>
+
+            <div className="fixed-div">
             <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
               onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              className="logout-btn"
             >
               Logout
             </button>
+            <Link to="/add" className="nav-add-btn">Add</Link>
+            </div>
           </div>
         </div>
       </div>
