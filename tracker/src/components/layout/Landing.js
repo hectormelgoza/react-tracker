a1f5ff871743aec3282b7bd2b8af2126a21ee468
Hector Melgoza <h.melgoza.4@gmail.com>
2020-08-27 23:32:29 -0700

    landing login button

--- tracker/src/components/layout/Landing.js
+++ tracker/src/components/layout/Landing.js
@@ -7,13 +7,15 @@ class Landing extends Component {
     return (
       
       <div className="landing-container">
+        <div className="landing-header">
         <h7>PurpleTabs</h7>
-    
+        <Link className="home-login-btn" to="/login">Log in</Link>
+        </div>
         <div>
           <div className="">
             <div className="message-brd">
             <h4>NEVER FORGET ANOTHER PASSWORD</h4>
-            <h8>GENERATE ACCOUNTS AND SAVE PASSWORDS</h8>
+            <h4>GENERATE ACCOUNTS AND SAVE PASSWORDS</h4>
             
               <Link className="get-started-btn" to="/register">Get Started!</Link>
             </div>
