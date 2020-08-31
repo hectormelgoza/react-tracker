758aa31a145773cfd4000d7d389a7ae49c3c6394
Hector Melgoza <h.melgoza.4@gmail.com>
2020-08-30 23:10:42 -0700

    nested document in user schema

--- tracker/backend/models/user.model.js
+++ tracker/backend/models/user.model.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-
+let Account = require('./account.model');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  date: { type: Date, required: true }
+  date: { type: Date, required: true },
+  accounts: [Account]
 }, {
   timestamps: true
-});
+})
 
 const User = mongoose.model('user-ids', userSchema);
 
