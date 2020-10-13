99c0ee90eab8c94c4ce219448ead33c3cb31d5ac
Hector Melgoza <h.melgoza.4@gmail.com>
2020-10-12 22:44:02 -0700

    register form simplified

--- tracker/src/components/auth/Register.js
+++ tracker/src/components/auth/Register.js
@@ -56,80 +56,72 @@ class Register extends Component {
     const { errors } = this.state;
     return (
       <div className="register-container">
-        <div className="">
-          <div className="">
-            <h4>Join today for free</h4>
-            <form noValidate onSubmit={this.onSubmit} className="login-form form-group">
-              <div className="">
-              <label className="label" htmlFor="name">Name<span className="red-text">{errors.name}</span></label><br></br>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="text"
+        <div className="sign-up-msg">
+          <h4>Join today for free</h4>
+        </div>
+
+        <form noValidate onSubmit={this.onSubmit} className="login-form form-group">
+          <label className="label" htmlFor="name">Name<span className="red-text">{errors.name}</span></label><br></br>
+          <input
+            onChange={this.onChange}
+            value={this.state.name}
+            error={errors.name}
+            id="name"
+            type="text"
                   /* className={classnames("", {
                     invalid: errors.name
                   })} */
-                  className="inputs"
-                />
-              </div>
-              <div className="">
-              <label className="label"htmlFor="email">Email<span className="red-text">{errors.email}</span></label><br></br>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
+            className="inputs"
+          />
+
+          <label className="label"htmlFor="email">Email<span className="red-text">{errors.email}</span></label><br></br>
+          <input
+            onChange={this.onChange}
+            value={this.state.email}
+            error={errors.email}
+            id="email"
+            type="email"
                   /* className={classnames("", {
                     invalid: errors.email
                   })} */
-                  className="inputs"
-                />
-              </div>
-              <div className="input-field col s12">
-              <label className="label" htmlFor="password">Password<span className="red-text">{errors.password}</span></label><br></br>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
+            className="inputs"
+          />
+        
+          <label className="label" htmlFor="password">Password<span className="red-text">{errors.password}</span></label><br></br>
+          <input
+            onChange={this.onChange}
+            value={this.state.password}
+            error={errors.password}
+            id="password"
+            type="password"
                   /* className={classnames("", {
                     invalid: errors.password
                   })} */
-                  className="inputs"
-                />
-              </div>
-              <div className="input-field col s12">
-              <label className="label" htmlFor="password2">Confirm Password<span className="red-text">{errors.password2}</span></label><br></br>
-                <input
-                  className="inputs"
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
+            className="inputs"
+          />
+
+          <label className="label" htmlFor="password2">Confirm Password<span className="red-text">{errors.password2}</span></label><br></br>
+          <input
+            className="inputs"
+            onChange={this.onChange}
+            value={this.state.password2}
+            error={errors.password2}
+            id="password2"
+            type="password"
                   /* className={classnames("", {
                     invalid: errors.password2
                   })} */
-                />
-              </div>
-              <div className="" >
-                <button
-                  type="submit"
-                  className="sign-up-btn"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
-            <p className="new-user">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
-          </div>
-        </div>
+          />
+            
+          <button type="submit" className="sign-up-btn">
+            Sign up
+          </button>
+              
+        </form>
+
+        <p className="new-user">
+          Already have an account? <Link to="/login">Log in</Link>
+        </p>
       </div>
     );
   }
