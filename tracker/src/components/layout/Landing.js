68dbd0912779149c2e4e19e9df24be4f89f6c558
Hector Melgoza <h.melgoza.4@gmail.com>
2020-10-13 00:23:34 -0700

    mobile landing temp

--- tracker/src/components/layout/Landing.js
+++ tracker/src/components/layout/Landing.js
@@ -5,27 +5,19 @@ import Logo from "../../images/pt.svg"
 class Landing extends Component {
   render() {
     return (
-      
       <div className="landing-container">
         <div className="landing-header">
-        <h7>PurpleTabs</h7>
-        <Link className="home-login-btn" to="/login">Log in</Link>
+          <h7>PurpleTabs</h7>
+          <Link className="home-login-btn" to="/login">Log in</Link>
         </div>
         
+        <div className="message-brd">
+          <h4>NEVER FORGET ANOTHER PASSWORD</h4>
+          <h4>GENERATE ACCOUNTS AND SAVE PASSWORDS</h4>
           
-            <div className="message-brd">
-            <h4>NEVER FORGET ANOTHER PASSWORD</h4>
-            <h4>GENERATE ACCOUNTS AND SAVE PASSWORDS</h4>
-            
-              <Link className="get-started-btn" to="/register">Get Started!</Link>
-            </div>
-          
-        
-        <div className="btt">
-
+          <Link className="get-started-btn" to="/register">Get Started!</Link>
         </div>
       </div>
-      
     );
   }
 }
