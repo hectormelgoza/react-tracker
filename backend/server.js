45e0c94453f8e80b5f176e3133e4337792e011dc
Hector Melgoza <h.melgoza.4@gmail.com>
2020-10-23 16:51:09 -0700

    5

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,9 @@ app.use('/api/users', users);
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
